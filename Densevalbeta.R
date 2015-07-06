d0d4d52481d63aca53f7873e07e28966f547aaee
sor16 <solviro@gmail.com>
2015-07-06 00:38:08 +0000

    made Densevalbeta faster

--- Densevalbeta.R
+++ Densevalbeta.R
@@ -1,4 +1,3 @@
-library(MASS)
 qvdata=read.table("V508.txt",skip=3,sep="|",dec=",")
 qvdata=qvdata[,c(2:4,7)]
 qvdata[,3:4]=qvdata[,4:3]
@@ -7,7 +6,7 @@ qvdata$Time=as.character(qvdata$Time)
 qvdata$Date=as.Date(gsub("\\.","-",qvdata$Date),"%d-%m-%Y")
 qvdata=qvdata[with(qvdata,order(W)),]
 wq=as.matrix(qvdata[,3:4])
-
+# dist fylki fyrir joint vigur
 Densevalmbeta <- function(th,RC,Wsim){ 
 zeta=th[1]
 phi_b=th[2]
@@ -15,47 +14,62 @@ sig_b2=th[3]
 lambda = th[4:9]
 varr_sim = c(exp(RC$Bsim %*% lambda))
 
-c= min(Wsim)-exp(zeta)
+c=min(Wsim)-exp(zeta)
 l=log(Wsim-c)
 m=length(Wsim)
 n=RC$n
-Wsim_rep=as.matrix(Wsim)%*%rep(1,m)
-dist2=abs(t(Wsim_rep)-Wsim_rep)
-sigma_11=sig_b2*(1+(sqrt(5)/exp(phi_b))*RC$dist+(5/(3*exp(phi_b)))*RC$dist^2)*exp(-(sqrt(5)/exp(phi_b))*RC$dist)
-sigma_22=sig_b2*(1+(sqrt(5)/exp(phi_b))*dist2+(5/(3*exp(phi_b)))*dist2^2)*exp(-(sqrt(5)/exp(phi_b))*dist2)
-dist3=sapply(unique(RC$w),function(x) abs(x-Wsim))
-sigma_21=sig_b2*(1+(sqrt(5)/exp(phi_b))*dist3+(5/(3*exp(phi_b)))*dist3^2)*exp(-(sqrt(5)/exp(phi_b))*dist3)
-sigma_12=t(sigma_21)
-mu_beta=sigma_21%*%t(sigma_11)%*%mu[3:length(mu)]
-Sigma=-(sigma_22-sigma_21%*%t(sigma_11)%*%sigma_12)
+#Wsim_rep=as.matrix(c(unique(RC$w),Wsim))%*%rep(1,m+n)
+#dist=abs(t(Wsim_rep)-Wsim_rep)
+W_all=c(RC$O,Wsim)
+dist=abs(outer(W_all,W_all,FUN="-"))
+sigma_all=sig_b2*(1+(sqrt(5)/exp(phi_b))*dist+(5/(3*exp(phi_b)))*dist^2)*exp(-(sqrt(5)/exp(phi_b))*dist) + diag(n+m) * RC$nugget
+sigma_11=sigma_all[1:n,1:n]
+#sigma_trick=sigma_11 +sqrt(varr_sim)*diag(rnorm(n))
+sigma_22=sigma_all[(n+1):(m+n),(n+1):(m+n)]
+sigma_12=sigma_all[1:n,(n+1):(n+m)]
+sigma_21=sigma_all[(n+1):(n+m),1:n]
+# Wsim_rep=as.matrix(Wsim)%*%rep(1,m)
+# dist2=abs(t(Wsim_rep)-Wsim_rep)
+# sigma_11a=sig_b2*(1+(sqrt(5)/exp(phi_b))*RC$dist+(5/(3*exp(phi_b)))*RC$dist^2)*exp(-(sqrt(5)/exp(phi_b))*RC$dist)
+# sigma_22a=sig_b2*(1+(sqrt(5)/exp(phi_b))*dist2+(5/(3*exp(phi_b)))*dist2^2)*exp(-(sqrt(5)/exp(phi_b))*dist2)
+# dist3=sapply(unique(RC$w),function(x) abs(x-Wsim))
+# sigma_21a=sig_b2*(1+(sqrt(5)/exp(phi_b))*dist3+(5/(3*exp(phi_b)))*dist3^2)*exp(-(sqrt(5)/exp(phi_b))*dist3)
+# sigma_12a=t(sigma_21a)
+
+
+
+mu_beta=sigma_21%*%solve(sigma_11,mu[3:length(mu)])
+Sigma=(sigma_22-sigma_21%*%solve(sigma_11,sigma_12))
 ncols <- ncol(Sigma)
-#mu <- rep(mu_beta, each = 1) ## not obliged to use a matrix (recycling)
-rbeta=as.numeric(mu_beta) + t(rnorm(ncols)) %*% chol(Sigma)
-
-X=rbind(rep(1,m),l,matrix(0,m,n),diag(l))
-x=rbind(mu[1],mu[2])
-#rbeta=mvrnorm(n=1,mu=mu_beta,Sigma=Sigma)
-
-#
-Sig_eps = diag(c(varr, 0))
-R_Beta = (1 + sqrt(5) * RC$dist/exp(phi_b) + 5 * RC$dist^2/(3 *exp(phi_b)^2)) * 
-    exp(-sqrt(5) * RC$dist/exp(phi_b)) +diag(RC$n) * RC$nugget
-
-Sig_x = rbind(cbind(RC$Sig_ab, RC$m1), cbind(RC$m2, exp(sig_b2) * R_Beta))
-
-L = t(chol(X %*% Sig_x %*% t(X) + Sig_eps))
-
-
-W = solve(L, X %*% Sig_x)
-x_u = RC$mu_x + t(chol(Sig_x)) %*% rnorm(RC$n + 2)
-sss = (X %*% x_u) - RC$y + rbind(sqrt(varr) * as.matrix(rnorm(RC$N)), 0)
-x = as.matrix(x_u - t(W) %*% solve(L, sss))
-yp = X %*% x
-yp = yp[1:RC$N, ]
-ypo = yp + as.matrix(rnorm(RC$N)) * sqrt(varr)
-#
+rbeta=as.numeric(mu_beta) + rnorm(ncols) %*% chol(Sigma)
 return(rbeta)
 }
+
+rbeta=mvrnorm(n=1,mu=mu_beta,Sigma=Sigma)
+rbeta=rmvn(n=1,mu=mu_beta,Sigma=Sigma)
+# 
+# #
+# Sig_eps = diag(c(varr, 0))
+# R_Beta = (1 + sqrt(5) * RC$dist/exp(phi_b) + 5 * RC$dist^2/(3 *exp(phi_b)^2)) * 
+#     exp(-sqrt(5) * RC$dist/exp(phi_b)) +diag(RC$n) * RC$nugget
+# 
+# Sig_x = rbind(cbind(RC$Sig_ab, RC$m1), cbind(RC$m2, exp(sig_b2) * R_Beta))
+# 
+# L = t(chol(X %*% Sig_x %*% t(X) + Sig_eps))
+# 
+# 
+# W = solve(L, X %*% Sig_x)
+# x_u = RC$mu_x + t(chol(Sig_x)) %*% rnorm(RC$n + 2)
+# sss = (X %*% x_u) - RC$y + rbind(sqrt(varr) * as.matrix(rnorm(RC$N)), 0)
+# x = as.matrix(x_u - t(W) %*% solve(L, sss))
+# yp = X %*% x
+# yp = yp[1:RC$N, ]
+# ypo = yp + as.matrix(rnorm(RC$N)) * sqrt(varr)
+return(rbeta)
+}
+
+
+
 
 
 
--- beta.R
+++ beta.R
@@ -1,60 +1,72 @@
+library(doParallel)
+library(foreach)
+library(RCmodels)
+Nit=20000
 
+RC=priors("Iceland")
 
-#t_median=t(apply(t, 1, quantile, probs = c(0.5),  na.rm = TRUE))
-#t is a 9 * 50000 matrix. MCMC for V508.txt
-t_median=c(-0.1391361,-2.275301, 0.4984314, -5.993821, -6.354939, -6.678067, -6.688161,-6.704671, -6.698719)
-#t is a 9 * 50000 matrix. MCMC for 15.txt
-#t_median=c(-1.630088, -2.313527, 0.4967125, -4.465228, -3.648009, -4.898924, -5.848946,-5.031323, -5.061868)
-t_m=t_median
-# t_m =Densmin$par
-# H=Densmin$hessian
+RC$nugget=10^-8
+RC$mu_sb=0.5
+RC$mu_pb=0.5
+RC$tau_pb2=0.25^2
+RC$s=3
+RC$v=5
 
+RC$y=rbind(as.matrix(log(wq[,2])),0)
+RC$w=as.matrix(0.01*wq[,1])
+RC$w_tild=RC$w-min(RC$w)
 
+Adist1 <- Adist(sort(RC$w))
+RC$A=Adist1$A
+RC$dist=Adist1$dist
+RC$n=Adist1$n
+RC$N=Adist1$N
+RC$O=Adist1$O
 
+RC$P=diag(nrow=5,ncol=5,6)-matrix(nrow=5,ncol=5,1)
+RC$Sig_ab= rbind(c(RC$sig_a^2, RC$p_ab*RC$sig_a*RC$sig_b), c(RC$p_ab*RC$sig_a*RC$sig_b, RC$sig_b^2))
+RC$mu_x=as.matrix(c(RC$mu_a,RC$mu_b, rep(0,RC$n))) #Setja i RC
+
+RC$B=B_splines(t(RC$w_tild)/RC$w_tild[length(RC$w_tild)])
+RC$Z=cbind(t(rep(0,2)),t(rep(1,RC$n)))
+
+RC$m1=matrix(0,nrow=2,ncol=RC$n)
+RC$m2=matrix(0,nrow=RC$n,ncol=2)
+theta.init=rep(0,9)
+
+Dens = function(th) {-Densevalm22(th,RC)$p}
+Densmin=optim(par=theta.init,Dens,method="BFGS",hessian=TRUE)
+t_m =Densmin$par
+H=Densmin$hessian
 phi_b=t_m[3]
 sig_b2=t_m[2]
 zeta=t_m[1]
 lambda=t_m[4:9]
-
 l=log(RC$w_tild+exp(t_m[1])) #as.matrix
-
 varr_m=exp(RC$B%*%lambda)
 Sig_eps=diag(as.numeric(rbind(varr_m,0)))
 R_Beta=(1+sqrt(5)*RC$dist/exp(phi_b)+5*RC$dist^2/(3*exp(phi_b)^2))*exp(-sqrt(5)*RC$dist/exp(phi_b))+diag(1,RC$n,RC$n)*RC$nugget
 Sig_x=rbind(cbind(RC$Sig_ab,matrix(0,nrow=2,ncol=RC$n)),cbind(matrix(0,nrow=RC$n,ncol=2),exp(sig_b2)*R_Beta))
 
-X=rbind(cbind(matrix(1,dim(l)),l,diag(as.numeric(l))%*%RC$A,RC$Z))
+X=rbind(cbind(matrix(1,dim(l)),l,diag(as.numeric(l))%*%RC$A),RC$Z)
 
 
 L=t(chol(as.matrix(X%*%Sig_x%*%t(X)+Sig_eps)))
 
 w=solve(L,(-RC$y+X%*%RC$mu_x))
 mu=RC$mu_x-Sig_x%*%(t(X)%*%(solve(t(L),w)))
-
-ymu=X%*%mu
-ymu=ymu[1:RC$N]
-plot(RC$w,exp(ymu))
-
-
-v=seq(min(wq[,1]),max(wq[,1]),length.out=40)
-seq=unique(c(wq[,1],v))
-seq=sort(seq)
-seq2=c(seq[2:length(seq)],1000)
-dist=abs(seq-seq2)
-mindist=0.1
-Wsim=0.01*seq[which(dist>mindist)]
-RC$w_tildsim=as.matrix(Wsim-min(Wsim))
-RC$Bsim=B_splines(t(RC$w_tildsim)/RC$w_tildsim[length(RC$w_tildsim)])
+LH=t(chol(H))/0.8
 
 cl <- makeCluster(4)
 # Register cluster
 registerDoParallel(cl)
 #Find out how many
+
 MCMC <- foreach(i=1:4, .combine=cbind,.export=c("Densevalm22")) %dopar% {
-    beta_u=matrix(0,nrow=length(Wsim),ncol=Nit)
+    ypo=matrix(0,nrow=nrow(wq)+9,ncol=Nit)
     
     t_old=as.matrix(t_m)
-    Dens<-Densevalmbeta(t_old,RC,Wsim)
+    Dens<-Densevalm22(t_old,RC)
     p_old=Dens$p
     ypo_old=Dens$ypo
     
@@ -74,12 +86,52 @@ MCMC <- foreach(i=1:4, .combine=cbind,.export=c("Densevalm22")) %dopar% {
         ypo[,j]=rbind(ypo_old,t_old)
     }
     
-    seq=seq(2000,Nit,5)
-    ypo=ypo[,seq]
+    #seq=seq(2000,Nit,5)
+    #ypo=ypo[,seq]
     
     return(ypo)
 }
 quantmatrix=head(MCMC,nrow(MCMC)-9)
 t=tail(MCMC,9)
+##########################
+#BETA_U
+##########################
+size_grid=50
+v=seq(min(wq[,1]),max(wq[,1]),length.out=size_grid)
+#seq=unique(c(wq[,1],v))
+Wsim=0.01*sort(v)
+#seq2=c(seq[2:length(seq)],1000)
+#filter=abs(seq-seq2)
+#mindist=0.1
+#Wsim=0.01*seq[which(filter>mindist)]
+#RC$w_tildsim=as.matrix(Wsim-min(Wsim))
+# v=seq(min(wq[,1]),max(wq[,1]),length.out=size_grid)
+# seq=unique(c(wq[,1],v))
+# seq=sort(seq)
+# seq2=c(seq[2:length(seq)],1000)
+# filter=abs(seq-seq2)
+# mindist=0.1
+# Wsim=0.01*seq[which(filter>mindist)]
+# RC$w_tildsim=as.matrix(Wsim-min(Wsim))
+RC$Bsim=B_splines(t(RC$w_tildsim)/RC$w_tildsim[length(RC$w_tildsim)])
 
+#MCMC <- foreach(i=1:4, .combine=cbind,.export=c("Densevalmbeta")) %dopar% {
+    beta_u=apply(t,2,FUN=function(x) Densevalmbeta(x,RC,Wsim=Wsim))
+    seq=seq(2000,Nit,5)
+    beta_u=beta_u[,seq]
+#}
+beta_u_median=apply(beta_u,1,quantile, probs = c(0.025,0.5, 0.975),na.rm=T)
+X=rbind(rep(1,m),l,matrix(0,m,n),diag(l))
+x=rbind(mu[1],mu[2])
+
+all_values <- function(x) {
+    if(is.null(x)) return(NULL)
+    row <- qvdata[data$id == x$id, ]
+    paste0(names(row), ": ", format(row), collapse = "<br />")
+}
+
+base <- data %>% ggvis(x = ~exp(Q), y = ~W,key:= ~id) %>%
+    layer_points() %>% add_tooltip(all_values, "hover")%>%layer_lines(x= ~exp(fit),y= ~W) %>%
+    layer_lines(x= ~exp(lower),y= ~W,strokeDash:=6)%>%layer_lines(x= ~exp(upper),y= ~W,strokeDash:=6)
+base
 
